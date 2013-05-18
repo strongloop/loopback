fc25f9579255b1c4550a814617275f598451578f
Ritchie Martori <skawful@gmail.com>
2013-05-17 17:56:55 -0700

    Update ignores

--- node_modules/sl-module-loader/example/sample-app/my-module-type.js
+++
@@ -1,9 +0,0 @@
-module.exports = MyModuleType;
-
-function MyModuleType(options) {
-  this.options = options;
-}
-
-MyModuleType.prototype.speak = function () {
-  console.log(this.options.msg || 'no message provided...');
-}
