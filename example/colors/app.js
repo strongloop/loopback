a6f0a8c41922b11a6760a0ab9b3b8307a5f891fc
Ritchie Martori <skawful@gmail.com>
2013-09-20 14:42:43 -0700

    fix the color example and remove old examples

--- example/colors/app.js
+++ example/colors/app.js
@@ -1,22 +1,52 @@
 var loopback = require('../../');
 var app = loopback();
+var MemoryConnector = loopback.Memory;
+
+/**
+ * Define a Color model
+ */
+
+var Color = loopback.Model.extend('color');
+
+/**
+ * Attach the Color model to a memory DataSource
+ */
+
+var MemoryDataSource = loopback.createDataSource({connector: MemoryConnector});
+Color.attachTo(MemoryDataSource);
+
+/**
+ * Insert some Color model instances into the memory database
+ */
+
+Color.create({
+  name: 'red'
+});
+
+Color.create({
+  name: 'blue'
+});
+
+Color.create({
+  name: 'green'
+});
+
+/**
+ * Query the colors using the REST API
+ */
+
+console.log('find the colors using REST:');
+console.log('\tcurl http://localhost:3000/colors');
+
+/*
+ * Expose models over REST 
+ */
 
 app.use(loopback.rest());
+app.model(Color);
 
-var schema = {
-  name: String
-};
-
-var Color = app.model('color', schema);
-
-app.dataSource('db', {adapter: 'memory'});
-
-Color.dataSource('db');
-
-Color.create({name: 'red'});
-Color.create({name: 'green'});
-Color.create({name: 'blue'});
+/**
+ * Start the server
+ */
 
 app.listen(3000);
-
-console.log('a list of colors is available at http://localhost:3000/colors');
