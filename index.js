ef7b724375d8c61b2d9abb21ad657100e1c903d2
Ritchie Martori <skawful@gmail.com>
2014-02-19 11:44:16 -0800

    Initial client-server example

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var datasourceJuggler = require('loopback-datasource-juggler');
 loopback.Connector = require('./lib/connectors/base-connector');
 loopback.Memory = require('./lib/connectors/memory');
 loopback.Mail = require('./lib/connectors/mail');
+loopback.Remote = require('./lib/connectors/remote');
 
 /**
  * Types
