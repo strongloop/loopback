dcdbef861e73b52a2718eb189afb3bbd2c740cc0
Ritchie Martori <skawful@gmail.com>
2014-02-19 17:09:36 -0800

    Add Remote connector

--- example/client-server/server.js
+++ example/client-server/server.js
@@ -11,14 +11,14 @@ server.model(CartItem);
 CartItem.attachTo(memory);
 
 // test data
-// CartItem.create([
-//   {item: 'red hat', qty: 6, price: 19.99, cartId: 1},
-//   {item: 'green shirt', qty: 1, price: 14.99, cartId: 1},
-//   {item: 'orange pants', qty: 58, price: 9.99, cartId: 1}
-// ]);
+CartItem.create([
+  {item: 'red hat', qty: 6, price: 19.99, cartId: 1},
+  {item: 'green shirt', qty: 1, price: 14.99, cartId: 1},
+  {item: 'orange pants', qty: 58, price: 9.99, cartId: 1}
+]);
 
-// CartItem.sum(1, function(err, total) {
-//   console.log(total);
-// })
+CartItem.sum(1, function(err, total) {
+  console.log(total);
+});
 
 server.listen(3000);
