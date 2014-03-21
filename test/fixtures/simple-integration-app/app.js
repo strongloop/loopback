de5d0b89494d96d6a6cdfeffa7f436a15b2c3e2b
Ritchie Martori <skawful@gmail.com>
2014-03-21 12:02:11 -0700

    Make verifications url safe

--- test/fixtures/simple-integration-app/app.js
+++ test/fixtures/simple-integration-app/app.js
@@ -7,7 +7,6 @@ app.use(loopback.favicon());
 app.use(loopback.cookieParser({secret: app.get('cookieSecret')}));
 var apiPath = '/api';
 app.use(apiPath, loopback.rest());
-app.use(app.router);
 app.use(loopback.static(path.join(__dirname, 'public')));
 app.use(loopback.urlNotFound());
 app.use(loopback.errorHandler());
