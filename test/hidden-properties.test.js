5b50a99eb366f2af36c6a5b6479adacc04fc1547
Ritchie Martori <skawful@gmail.com>
2014-03-21 12:53:04 -0700

    Add hidden property support to models

--- test/hidden-properties.test.js
+++ test/hidden-properties.test.js
@@ -4,15 +4,27 @@ describe('hidden properties', function () {
   beforeEach(function (done) {
     var app = this.app = loopback();
     var Product = this.Product = app.model('product', {
-      properties: {
-        secret: {}
-      },
+      options: {hidden: ['secret']},
       dataSource: loopback.memory()
     });
-    app.listen(done);
+    app.use(loopback.rest());
+
+    Product.create(
+      {name: 'pencil', secret: 'secret'},
+      done
+    );
   });
 
-  it('should hide a property remotely', function () {
-    
+  it('should hide a property remotely', function (done) {
+     request(this.app)
+        .get('/products')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res){
+          if(err) return done(err);
+          var product = res.body[0];
+          assert.equal(product.secret, undefined);
+          done();
+        });
   });
 });
