6cbc231fba442233b2afa91c628baa387b47c8c1
Miroslav Bajtoš <miroslav@strongloop.com>
2014-10-13 11:45:06 +0200

    models: move Checkpoint LDL def into a json file

--- common/models/checkpoint.js
+++ common/models/checkpoint.js
@@ -2,27 +2,7 @@
  * Module Dependencies.
  */
 
-var PersistedModel = require('../../lib/loopback').PersistedModel
-  , loopback = require('../../lib/loopback')
-  , assert = require('assert');
-
-/**
- * Properties
- */
-
-var properties = {
-  seq: {type: Number},
-  time: {type: Date, default: Date},
-  sourceId: {type: String}
-};
-
-/**
- * Options
- */
-
-var options = {
-
-};
+var assert = require('assert');
 
 /**
  * Checkpoint list entry.
@@ -30,48 +10,54 @@ var options = {
  * @property id {Number} the sequencial identifier of a checkpoint
  * @property time {Number} the time when the checkpoint was created
  * @property sourceId {String}  the source identifier
- * 
- * @class
+ *
+ * @class Checkpoint
  * @inherits {PersistedModel}
  */
 
-var Checkpoint = module.exports = PersistedModel.extend('Checkpoint', properties, options);
+module.exports = function(Checkpoint) {
 
-/**
- * Get the current checkpoint id
- * @callback {Function} callback
- * @param {Error} err
- * @param {Number} checkpointId The current checkpoint id
- */
+  // Workaround for https://github.com/strongloop/loopback/issues/292
+  Checkpoint.definition.rawProperties.time.default =
+    Checkpoint.definition.properties.time.default = function() {
+      return new Date();
+    };
 
-Checkpoint.current = function(cb) {
-  var Checkpoint = this;
-  this.find({
-    limit: 1,
-    order: 'seq DESC'
-  }, function(err, checkpoints) {
-    if(err) return cb(err);
-    var checkpoint = checkpoints[0];
-    if(checkpoint) {
-      cb(null, checkpoint.seq);
-    } else {
-      Checkpoint.create({seq: 0}, function(err, checkpoint) {
-        if(err) return cb(err);
+  /**
+   * Get the current checkpoint id
+   * @callback {Function} callback
+   * @param {Error} err
+   * @param {Number} checkpointId The current checkpoint id
+   */
+
+  Checkpoint.current = function(cb) {
+    var Checkpoint = this;
+    this.find({
+      limit: 1,
+      order: 'seq DESC'
+    }, function(err, checkpoints) {
+      if (err) return cb(err);
+      var checkpoint = checkpoints[0];
+      if (checkpoint) {
         cb(null, checkpoint.seq);
-      });
-    }
-  });
-}
-
-Checkpoint.beforeSave = function(next, model) {
-  if(!model.getId() && model.seq === undefined) {
-    model.constructor.current(function(err, seq) {
-      if(err) return next(err);
-      model.seq = seq + 1;
-      next();
+      } else {
+        Checkpoint.create({seq: 0}, function(err, checkpoint) {
+          if (err) return cb(err);
+          cb(null, checkpoint.seq);
+        });
+      }
     });
-  } else {
-    next();
   }
-}
 
+  Checkpoint.beforeSave = function(next, model) {
+    if (!model.getId() && model.seq === undefined) {
+      model.constructor.current(function(err, seq) {
+        if (err) return next(err);
+        model.seq = seq + 1;
+        next();
+      });
+    } else {
+      next();
+    }
+  }
+};
