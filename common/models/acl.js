ef890d5f2639d7642720651f5f7daa9ef94d6813
Miroslav Bajtoš <miroslav@strongloop.com>
2014-10-13 10:46:55 +0200

    models: move Scope def into its own files

--- common/models/acl.js
+++ common/models/acl.js
@@ -468,47 +468,4 @@ ACL.checkAccessForToken = function (token, model, modelId, method, callback) {
   });
 };
 
-/*!
- * Schema for Scope which represents the permissions that are granted to client
- * applications by the resource owner
- */
-var ScopeSchema = {
-  name: {type: String, required: true},
-  description: String
-};
-
-/**
- * Resource owner grants/delegates permissions to client applications
- *
- * For a protected resource, does the client application have the authorization
- * from the resource owner (user or system)?
- *
- * Scope has many resource access entries
- * @class
- */
-var Scope = loopback.createModel('Scope', ScopeSchema);
-
-
-/**
- * Check if the given scope is allowed to access the model/property
- * @param {String} scope The scope name
- * @param {String} model The model name
- * @param {String} property The property/method/relation name
- * @param {String} accessType The access type
- * @callback {Function} callback
- * @param {String|Error} err The error object
- * @param {AccessRequest} result The access permission
- */
-Scope.checkPermission = function (scope, model, property, accessType, callback) {
-  this.findOne({where: {name: scope}}, function (err, scope) {
-    if (err) {
-      callback && callback(err);
-    } else {
-      var aclModel = loopback.getModelByType(ACL);
-      aclModel.checkPermission(ACL.SCOPE, scope.id, model, property, accessType, callback);
-    }
-  });
-};
-
 module.exports.ACL = ACL;
-module.exports.Scope = Scope;
