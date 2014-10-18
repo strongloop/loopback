95e4d1f061ccace57f6ef19b96e43c7e8b4e5f93
Tony Sukiennik <tony@iRelate.us>
2014-10-18 16:02:59 -0700

    Update user.js

--- common/models/user.js
+++ common/models/user.js
@@ -233,7 +233,7 @@ User.prototype.verify = function(options, fn) {
   options.protocol = options.protocol || 'http';
 
   var app = userModel.app;
-  options.host = options.host || (app && app.get('host')) || 'localhost';
+  options.host = emailOptions.host || (app && app.get('verificationHost')) || (app && app.get('host')) || 'localhost';
   options.port = options.port || (app && app.get('port')) || 3000;
   options.restApiRoot = options.restApiRoot || (app && app.get('restApiRoot')) || '/api';
   options.verifyHref = options.verifyHref ||
