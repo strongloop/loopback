4ee7b5f440face19fa1b86daa1edc199bbb53d29
Rob Halff <rob.halff@gmail.com>
2014-11-21 02:47:47 +0100

    'memory' is already defined

--- test/memory.test.js
+++ test/memory.test.js
@@ -6,25 +6,25 @@ describe('Memory Connector', function(){
 
     // or create it using the standard
     // data source creation api
-    var memory = loopback.createDataSource({
+    memory = loopback.createDataSource({
       connector: loopback.Memory
     });
-    
+
     // create a model using the
     // memory data source
     var properties = {
       name: String,
       price: Number
     };
-    
+
     var Product = memory.createModel('product', properties);
-    
+
     Product.create([
       {name: 'apple', price: 0.79},
       {name: 'pear', price: 1.29},
       {name: 'orange', price: 0.59},
     ], count);
-    
+
     function count() {
       Product.count(function (err, count) {
         assert.equal(count, 3);
