strongloop/loopback

commit b9170751bcd776360df9854fc8fd87b7ebe3fddb

Add support for app level Model isolation
 - `loopback.registry` is now a true global registry
 - `app.registry` is unique per app object
 - `Model.registry` is set when a Model is created using any registry method
 - `loopback.localRegistry` and `loopback({localRegistry: true})` when set to `true` this will create a `Registry` per `Application`. It defaults to `false`.

lib/loopback.js
@@ -10,6 +10,9 @@ var ejs = require('ejs');
 var path = require('path');
 var merge = require('util')._extend;
 var assert = require('assert');
+var Registry = require('./registry');
+var getGlobalRegistry = require('./global-registry');
+var juggler = require('loopback-datasource-juggler');
 
 /**
  * LoopBack core module. It provides static properties and
@@ -25,6 +28,7 @@ var assert = require('assert');
  * @property {String} mime
  * @property {Boolean} isBrowser True if running in a browser environment; false otherwise.  Static read-only property.
  * @property {Boolean} isServer True if running in a server environment; false otherwise.  Static read-only property.
+ * @property {Registry} registry The global `Registry` object.
  * @property {String} faviconFile Path to a default favicon shipped with LoopBack.
  * Use as follows: `app.use(require('serve-favicon')(loopback.faviconFile));`
  * @class loopback
@@ -45,6 +49,22 @@ loopback.version = require('../package.json').version;
 
 loopback.mime = express.mime;
 
+Object.defineProperty(loopback, 'registry', {
+  get: getGlobalRegistry
+});
+
+Object.defineProperty(loopback, 'Model', {
+  get: function() {
+    return this.registry.getModel('Model');
+  }
+});
+
+Object.defineProperty(loopback, 'PersistedModel', {
+  get: function() {
+    return this.registry.getModel('PersistedModel');
+  }
+});
+
 /*!
  * Create an loopback application.
  *
@@ -52,7 +72,7 @@ loopback.mime = express.mime;
  * @api public
  */
 
-function createApplication() {
+function createApplication(options) {
   var app = loopbackExpress();
 
   merge(app, proto);
@@ -76,6 +96,13 @@ function createApplication() {
   app.connector('memory', loopback.Memory);
   app.connector('remote', loopback.Remote);
 
+  if (loopback.localRegistry || options && options.localRegistry === true) {
+    // setup the app registry
+    var registry = app.registry = new Registry();
+  } else {
+    app.registry = loopback.registry;
+  }
+
   return app;
 }
 
@@ -91,7 +118,6 @@ function mixin(source) {
 }
 
 mixin(require('./runtime'));
-mixin(require('./registry'));
 
 /*!
  * Expose static express methods like `express.errorHandler`.
@@ -191,8 +217,198 @@ loopback.template = function(file) {
 
 require('../server/current-context')(loopback);
 
+/**
+ * Create a named vanilla JavaScript class constructor with an attached
+ * set of properties and options.
+ *
+ * This function comes with two variants:
+ *  * `loopback.createModel(name, properties, options)`
+ *  * `loopback.createModel(config)`
+ *
+ * In the second variant, the parameters `name`, `properties` and `options`
+ * are provided in the config object. Any additional config entries are
+ * interpreted as `options`, i.e. the following two configs are identical:
+ *
+ * ```js
+ * { name: 'Customer', base: 'User' }
+ * { name: 'Customer', options: { base: 'User' } }
+ * ```
+ *
+ * **Example**
+ *
+ * Create an `Author` model using the three-parameter variant:
+ *
+ * ```js
+ * loopback.createModel(
+ *   'Author',
+ *   {
+ *     firstName: 'string',
+ *     lastName: 'string'
+ *   },
+ *   {
+ *     relations: {
+ *       books: {
+ *         model: 'Book',
+ *         type: 'hasAndBelongsToMany'
+ *       }
+ *     }
+ *   }
+ * );
+ * ```
+ *
+ * Create the same model using a config object:
+ *
+ * ```js
+ * loopback.createModel({
+ *   name: 'Author',
+ *   properties: {
+ *     firstName: 'string',
+ *     lastName: 'string'
+ *   },
+ *   relations: {
+ *     books: {
+ *       model: 'Book',
+ *       type: 'hasAndBelongsToMany'
+ *     }
+ *   }
+ * });
+ * ```
+ *
+ * @param {String} name Unique name.
+ * @param {Object} properties
+ * @param {Object} options (optional)
+ *
+ * @header loopback.createModel
+ */
+
+loopback.createModel = function(name, properties, options) {
+  return this.registry.createModel.apply(this.registry, arguments);
+};
+
+/**
+ * Alter an existing Model class.
+ * @param {Model} ModelCtor The model constructor to alter.
+ * @options {Object} config Additional configuration to apply
+ * @property {DataSource} dataSource Attach the model to a dataSource.
+ * @property {Object} [relations] Model relations to add/update.
+ *
+ * @header loopback.configureModel(ModelCtor, config)
+ */
+
+loopback.configureModel = function(ModelCtor, config) {
+  return this.registry.configureModel.apply(this.registry, arguments);
+};
+
+/**
+ * Look up a model class by name from all models created by
+ * `loopback.createModel()`
+ * @param {String} modelName The model name
+ * @returns {Model} The model class
+ *
+ * @header loopback.findModel(modelName)
+ */
+loopback.findModel = function(modelName) {
+  return this.registry.findModel.apply(this.registry, arguments);
+};
+
+/**
+ * Look up a model class by name from all models created by
+ * `loopback.createModel()`. Throw an error when no such model exists.
+ *
+ * @param {String} modelName The model name
+ * @returns {Model} The model class
+ *
+ * @header loopback.getModel(modelName)
+ */
+loopback.getModel = function(modelName) {
+  return this.registry.getModel.apply(this.registry, arguments);
+};
+
+/**
+ * Look up a model class by the base model class.
+ * The method can be used by LoopBack
+ * to find configured models in models.json over the base model.
+ * @param {Model} modelType The base model class
+ * @returns {Model} The subclass if found or the base class
+ *
+ * @header loopback.getModelByType(modelType)
+ */
+loopback.getModelByType = function(modelType) {
+  return this.registry.getModelByType.apply(this.registry, arguments);
+};
+
+/**
+ * Create a data source with passing the provided options to the connector.
+ *
+ * @param {String} name Optional name.
+ * @options {Object} options Data Source options
+ * @property {Object} connector LoopBack connector.
+ * @property {*} [*] Other&nbsp;connector properties.
+ *   See the relevant connector documentation.
+ */
+
+loopback.createDataSource = function(name, options) {
+  return this.registry.createDataSource.apply(this.registry, arguments);
+};
+
+/**
+ * Get an in-memory data source. Use one if it already exists.
+ *
+ * @param {String} [name] The name of the data source.
+ * If not provided, the `'default'` is used.
+ */
+
+loopback.memory = function(name) {
+  return this.registry.memory.apply(this.registry, arguments);
+};
+
+/**
+ * Set the default `dataSource` for a given `type`.
+ * @param {String} type The datasource type.
+ * @param {Object|DataSource} dataSource The data source settings or instance
+ * @returns {DataSource} The data source instance.
+ *
+ * @header loopback.setDefaultDataSourceForType(type, dataSource)
+ */
+
+loopback.setDefaultDataSourceForType = function(type, dataSource) {
+  return this.registry.setDefaultDataSourceForType.apply(this.registry, arguments);
+};
+
+/**
+ * Get the default `dataSource` for a given `type`.
+ * @param {String} type The datasource type.
+ * @returns {DataSource} The data source instance
+ */
+
+loopback.getDefaultDataSourceForType = function(type) {
+  return this.registry.getDefaultDataSourceForType.apply(this.registry, arguments);
+};
+
+/**
+ * Attach any model that does not have a dataSource to
+ * the default dataSource for the type the Model requests
+ */
+
+loopback.autoAttach = function() {
+  return this.registry.autoAttach.apply(this.registry, arguments);
+};
+
+loopback.autoAttachModel = function(ModelCtor) {
+  return this.registry.autoAttachModel.apply(this.registry, arguments);
+};
+
+// temporary alias to simplify migration of code based on <=2.0.0-beta3
+Object.defineProperty(loopback, 'DataModel', {
+  get: function() {
+    return this.registry.DataModel;
+  }
+});
+
 /*!
  * Built in models / services
  */
 
 require('./builtin-models')(loopback);
+
+loopback.DataSource = juggler.DataSource;

common/models/acl.js
@@ -375,6 +375,8 @@ module.exports = function(ACL) {
    */
 
   ACL.checkAccessForContext = function(context, callback) {
+    var registry = this.registry;
+
     if (!(context instanceof AccessContext)) {
       context = new AccessContext(context);
     }
@@ -394,7 +396,7 @@ module.exports = function(ACL) {
     var staticACLs = this.getStaticACLs(model.modelName, property);
 
     var self = this;
-    var roleModel = loopback.getModelByType(Role);
+    var roleModel = registry.getModelByType(Role);
     this.find({where: {model: model.modelName, property: propertyQuery,
       accessType: accessTypeQuery}}, function(err, acls) {
       if (err) {

common/models/role-mapping.js
@@ -24,9 +24,11 @@ module.exports = function(RoleMapping) {
    * @param {Application} application
    */
   RoleMapping.prototype.application = function(callback) {
+    var registry = this.constructor.registry;
+
     if (this.principalType === RoleMapping.APPLICATION) {
       var applicationModel = this.constructor.Application ||
-        loopback.getModelByType(loopback.Application);
+        registry.getModelByType('Application');
       applicationModel.findById(this.principalId, callback);
     } else {
       process.nextTick(function() {
@@ -42,9 +44,10 @@ module.exports = function(RoleMapping) {
    * @param {User} user
    */
   RoleMapping.prototype.user = function(callback) {
+    var RoleMapping = this.constructor;
     if (this.principalType === RoleMapping.USER) {
-      var userModel = this.constructor.User ||
-        loopback.getModelByType(loopback.User);
+      var userModel = RoleMapping.User ||
+        RoleMapping.registry.getModelByType('User');
       userModel.findById(this.principalId, callback);
     } else {
       process.nextTick(function() {
@@ -60,9 +63,11 @@ module.exports = function(RoleMapping) {
    * @param {User} childUser
    */
   RoleMapping.prototype.childRole = function(callback) {
+    var registry = this.constructor.registry;
+
     if (this.principalType === RoleMapping.ROLE) {
       var roleModel = this.constructor.Role ||
-        loopback.getModelByType(loopback.Role);
+        registry.getModelByType(loopback.Role);
       roleModel.findById(this.principalId, callback);
     } else {
       process.nextTick(function() {

common/models/role.js
@@ -29,7 +29,8 @@ module.exports = function(Role) {
 
   // Set up the connection to users/applications/roles once the model
   Role.once('dataSourceAttached', function() {
-    var roleMappingModel = this.RoleMapping || loopback.getModelByType(RoleMapping);
+    var registry = Role.registry;
+    var roleMappingModel = this.RoleMapping || registry.getModelByType(RoleMapping);
     Role.prototype.users = function(callback) {
       roleMappingModel.find({where: {roleId: this.id,
         principalType: RoleMapping.USER}}, function(err, mappings) {
@@ -242,6 +243,8 @@ module.exports = function(Role) {
       context = new AccessContext(context);
     }
 
+    var registry = this.registry;
+
     debug('isInRole(): %s', role);
     context.debug();
 
@@ -277,7 +280,7 @@ module.exports = function(Role) {
       return;
     }
 
-    var roleMappingModel = this.RoleMapping || loopback.getModelByType(RoleMapping);
+    var roleMappingModel = this.RoleMapping || registry.getModelByType(RoleMapping);
     this.findOne({where: {name: role}}, function(err, result) {
       if (err) {
         if (callback) callback(err);
@@ -332,6 +335,7 @@ module.exports = function(Role) {
       context = new AccessContext(context);
     }
     var roles = [];
+    var registry = this.registry;
 
     var addRole = function(role) {
       if (role && roles.indexOf(role) === -1) {
@@ -358,7 +362,7 @@ module.exports = function(Role) {
       });
     });
 
-    var roleMappingModel = this.RoleMapping || loopback.getModelByType(RoleMapping);
+    var roleMappingModel = this.RoleMapping || registry.getModelByType(RoleMapping);
     context.principals.forEach(function(p) {
       // Check against the role mappings
       var principalType = p.type || undefined;

common/models/scope.js
@@ -25,6 +25,7 @@ module.exports = function(Scope) {
    */
   Scope.checkPermission = function(scope, model, property, accessType, callback) {
     var ACL = loopback.ACL;
+    var registry = this.registry;
     assert(ACL,
       'ACL model must be defined before Scope.checkPermission is called');
 
@@ -32,7 +33,7 @@ module.exports = function(Scope) {
       if (err) {
         if (callback) callback(err);
       } else {
-        var aclModel = loopback.getModelByType(ACL);
+        var aclModel = registry.getModelByType(ACL);
         aclModel.checkPermission(ACL.SCOPE, scope.id, model, property, accessType, callback);
       }
     });

common/models/user.js
@@ -334,6 +334,7 @@ module.exports = function(User) {
   User.prototype.verify = function(options, fn) {
     var user = this;
     var userModel = this.constructor;
+    var registry = userModel.registry;
     assert(typeof options === 'object', 'options required when calling user.verify()');
     assert(options.type, 'You must supply a verification type (options.type)');
     assert(options.type === 'email', 'Unsupported verification type');
@@ -364,7 +365,7 @@ module.exports = function(User) {
       options.redirect;
 
     // Email model
-    var Email = options.mailer || this.constructor.email || loopback.getModelByType(loopback.Email);
+    var Email = options.mailer || this.constructor.email || registry.getModelByType(loopback.Email);
 
     // Set a default token generation function if one is not provided
     var tokenGenerator = options.generateVerificationToken || User.generateVerificationToken;

lib/application.js
@@ -3,7 +3,7 @@
  */
 
 var DataSource = require('loopback-datasource-juggler').DataSource;
-var registry = require('./registry');
+var Registry = require('./registry');
 var assert = require('assert');
 var fs = require('fs');
 var extend = require('util')._extend;
@@ -104,6 +104,8 @@ app.disuse = function(route) {
 
 app.model = function(Model, config) {
   var isPublic = true;
+  var registry = this.registry;
+
   if (arguments.length > 1) {
     config = config || {};
     if (typeof Model === 'string') {
@@ -130,7 +132,7 @@ app.model = function(Model, config) {
     configureModel(Model, config, this);
     isPublic = config.public !== false;
   } else {
-    assert(Model.prototype instanceof registry.Model,
+    assert(Model.prototype instanceof Model.registry.getModel('Model'),
       Model.modelName + ' must be a descendant of loopback.Model');
   }
 
@@ -216,7 +218,7 @@ app.models = function() {
  * @param {Object} config The data source config
  */
 app.dataSource = function(name, config) {
-  var ds = dataSourcesFromConfig(config, this.connectors);
+  var ds = dataSourcesFromConfig(name, config, this.connectors, this.registry);
   this.dataSources[name] =
   this.dataSources[classify(name)] =
   this.dataSources[camelize(name)] = ds;
@@ -362,14 +364,14 @@ app.boot = function(options) {
     '`app.boot` was removed, use the new module loopback-boot instead');
 };
 
-function dataSourcesFromConfig(config, connectorRegistry) {
+function dataSourcesFromConfig(name, config, connectorRegistry, registry) {
   var connectorPath;
 
   assert(typeof config === 'object',
     'cannont create data source without config object');
 
   if (typeof config.connector === 'string') {
-    var name = config.connector;
+    name = config.connector;
     if (connectorRegistry[name]) {
       config.connector = connectorRegistry[name];
     } else {
@@ -385,7 +387,7 @@ function dataSourcesFromConfig(config, connectorRegistry) {
 }
 
 function configureModel(ModelCtor, config, app) {
-  assert(ModelCtor.prototype instanceof registry.Model,
+  assert(ModelCtor.prototype instanceof ModelCtor.registry.getModel('Model'),
     ModelCtor.modelName + ' must be a descendant of loopback.Model');
 
   var dataSource = config.dataSource;
@@ -405,7 +407,7 @@ function configureModel(ModelCtor, config, app) {
   config = extend({}, config);
   config.dataSource = dataSource;
 
-  registry.configureModel(ModelCtor, config);
+  app.registry.configureModel(ModelCtor, config);
 }
 
 function clearHandlerCache(app) {

lib/builtin-models.js
@@ -1,43 +1,43 @@
-module.exports = function(loopback) {
+module.exports = function(registry) {
   // NOTE(bajtos) we must use static require() due to browserify limitations
 
-  loopback.Email = createModel(
+  registry.Email = createModel(
     require('../common/models/email.json'),
     require('../common/models/email.js'));
 
-  loopback.Application = createModel(
+  registry.Application = createModel(
     require('../common/models/application.json'),
     require('../common/models/application.js'));
 
-  loopback.AccessToken = createModel(
+  registry.AccessToken = createModel(
     require('../common/models/access-token.json'),
     require('../common/models/access-token.js'));
 
-  loopback.RoleMapping = createModel(
+  registry.RoleMapping = createModel(
     require('../common/models/role-mapping.json'),
     require('../common/models/role-mapping.js'));
 
-  loopback.Role = createModel(
+  registry.Role = createModel(
     require('../common/models/role.json'),
     require('../common/models/role.js'));
 
-  loopback.ACL = createModel(
+  registry.ACL = createModel(
     require('../common/models/acl.json'),
     require('../common/models/acl.js'));
 
-  loopback.Scope = createModel(
+  registry.Scope = createModel(
     require('../common/models/scope.json'),
     require('../common/models/scope.js'));
 
-  loopback.User = createModel(
+  registry.User = createModel(
     require('../common/models/user.json'),
     require('../common/models/user.js'));
 
-  loopback.Change = createModel(
+  registry.Change = createModel(
     require('../common/models/change.json'),
     require('../common/models/change.js'));
 
-  loopback.Checkpoint = createModel(
+  registry.Checkpoint = createModel(
     require('../common/models/checkpoint.json'),
     require('../common/models/checkpoint.js'));
 
@@ -50,18 +50,18 @@ module.exports = function(loopback) {
     MAIL: 'mail'
   };
 
-  loopback.Email.autoAttach = dataSourceTypes.MAIL;
-  loopback.PersistedModel.autoAttach = dataSourceTypes.DB;
-  loopback.User.autoAttach = dataSourceTypes.DB;
-  loopback.AccessToken.autoAttach = dataSourceTypes.DB;
-  loopback.Role.autoAttach = dataSourceTypes.DB;
-  loopback.RoleMapping.autoAttach = dataSourceTypes.DB;
-  loopback.ACL.autoAttach = dataSourceTypes.DB;
-  loopback.Scope.autoAttach = dataSourceTypes.DB;
-  loopback.Application.autoAttach = dataSourceTypes.DB;
+  registry.Email.autoAttach = dataSourceTypes.MAIL;
+  registry.getModel('PersistedModel').autoAttach = dataSourceTypes.DB;
+  registry.User.autoAttach = dataSourceTypes.DB;
+  registry.AccessToken.autoAttach = dataSourceTypes.DB;
+  registry.Role.autoAttach = dataSourceTypes.DB;
+  registry.RoleMapping.autoAttach = dataSourceTypes.DB;
+  registry.ACL.autoAttach = dataSourceTypes.DB;
+  registry.Scope.autoAttach = dataSourceTypes.DB;
+  registry.Application.autoAttach = dataSourceTypes.DB;
 
   function createModel(definitionJson, customizeFn) {
-    var Model = loopback.createModel(definitionJson);
+    var Model = registry.createModel(definitionJson);
     customizeFn(Model);
     return Model;
   }

commit 4d0a824757ff6df81d80c8143d5434847d1456ee

Add back loopback properties like modelBuilder
The commit b917075 accidentally removed a couple of properties,
this commit is bringing them back:

 - loopback.modelRegistry
 - loopback.defaultDataSources

A unit-test was added to prevent this kind of regressions in the future.

lib/loopback.js
@@ -49,19 +49,21 @@ loopback.version = require('../package.json').version;
 
 loopback.mime = express.mime;
 
-Object.defineProperty(loopback, 'registry', {
-  get: getGlobalRegistry
-});
-
-Object.defineProperty(loopback, 'Model', {
-  get: function() {
-    return this.registry.getModel('Model');
-  }
-});
-
-Object.defineProperty(loopback, 'PersistedModel', {
-  get: function() {
-    return this.registry.getModel('PersistedModel');
+Object.defineProperties(loopback, {
+  registry: {
+    get: getGlobalRegistry
+  },
+  Model: {
+    get: function() { return this.registry.getModel('Model'); }
+  },
+  PersistedModel: {
+    get: function() { return this.registry.getModel('PersistedModel'); }
+  },
+  defaultDataSources: {
+    get: function() { return this.registry.defaultDataSources; }
+  },
+  modelBuilder: {
+    get: function() { return this.registry.modelBuilder; }
   }
 });
 
@@ -399,6 +401,7 @@ loopback.autoAttachModel = function(ModelCtor) {
 };
 
 // temporary alias to simplify migration of code based on <=2.0.0-beta3
+// TODO(bajtos) Remove this in v3.0
 Object.defineProperty(loopback, 'DataModel', {
   get: function() {
     return this.registry.DataModel;

test/loopback.test.js
@@ -27,6 +27,92 @@ describe('loopback', function() {
     it.onServer('has `getCurrentContext` method', function() {
       expect(loopback.getCurrentContext).to.be.a('function');
     });
+
+    it.onServer('exports all expected properties', function() {
+      var EXPECTED = [
+        'ACL',
+        'AccessToken',
+        'Application',
+        'Change',
+        'Checkpoint',
+        'Connector',
+        'DataModel',
+        'DataSource',
+        'Email',
+        'GeoPoint',
+        'Mail',
+        'Memory',
+        'Model',
+        'PersistedModel',
+        'Remote',
+        'Role',
+        'RoleMapping',
+        'Route',
+        'Router',
+        'Scope',
+        'User',
+        'ValidationError',
+        'application',
+        'arguments',
+        'autoAttach',
+        'autoAttachModel',
+        'bodyParser',
+        'caller',
+        'compress',
+        'configureModel',
+        'context',
+        'cookieParser',
+        'cookieSession',
+        'createContext',
+        'createDataSource',
+        'createModel',
+        'csrf',
+        'defaultDataSources',
+        'directory',
+        'errorHandler',
+        'favicon',
+        'faviconFile',
+        'findModel',
+        'getCurrentContext',
+        'getDefaultDataSourceForType',
+        'getModel',
+        'getModelByType',
+        'isBrowser',
+        'isServer',
+        'json',
+        'length',
+        'logger',
+        'memory',
+        'methodOverride',
+        'mime',
+        'modelBuilder',
+        'name',
+        'prototype',
+        'query',
+        'registry',
+        'remoteMethod',
+        'request',
+        'response',
+        'responseTime',
+        'rest',
+        'runInContext',
+        'session',
+        'setDefaultDataSourceForType',
+        'static',
+        'status',
+        'template',
+        'timeout',
+        'token',
+        'urlNotFound',
+        'urlencoded',
+        'version',
+        'vhost'
+      ];
+
+      var actual = Object.getOwnPropertyNames(loopback);
+      actual.sort();
+      expect(actual).to.eql(EXPECTED);
+    });
   });
 
   describe('loopback.createDataSource(options)', function() {