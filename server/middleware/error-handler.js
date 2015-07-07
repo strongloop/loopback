5bfed4dad018c455e486fe0bcb5177ee64dc5863
Richard Walker <digitalsadhu@gmail.com>
2015-07-07 21:13:50 +0200

    Replace env with options configuration
    Instead of removing stack when NODE_ENV=production,
    instead remove stack when options.includeStack === false
    Signed-off-by: Richard Walker <digitalsadhu@gmail.com>

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -3,12 +3,13 @@ expressErrorHandler.title = 'Loopback';
 
 module.exports = errorHandler;
 
-function errorHandler() {
-  if (process.env.NODE_ENV === 'production') {
+function errorHandler(options) {
+  if (options.includeStack !== false) {
+    return expressErrorHandler();
+  } else {
     return function errorHandler(err, req, res, next) {
       delete err.stack;
       return expressErrorHandler()(err, req, res, next);
     };
   }
-  return expressErrorHandler();
 }
