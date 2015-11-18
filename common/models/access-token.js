1c7b556191a28bc5de4aa58f0022ea8141b3e953
Jesús Carrera <jesus.carrera@frontendmatters.com>
2015-11-18 16:27:46 +0100

    remove console log

--- common/models/access-token.js
+++ common/models/access-token.js
@@ -146,7 +146,6 @@ module.exports = function(AccessToken) {
       var created = this.created.getTime();
       var elapsedSeconds = (now - created) / 1000;
       var secondsToLive = this.ttl;
-      console.log(this.constructor.settings);
       var isValid = (secondsToLive === -1 && this.constructor.settings.allowEternalTtl) || elapsedSeconds < secondsToLive;
 
       if (isValid) {
