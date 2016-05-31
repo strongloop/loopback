8d295b70f63ee572323f1f4b2dadcf36debbe776
Miroslav Bajtoš <mbajtos@cz.ibm.com>
2016-05-31 16:50:04 +0200

    Remove legacy express 3.x middleware getters
    Remove middleware-getter properties that were simlifying upgrade from
    LoopBack 1.x/Express 3.x applications:
    
      - loopback.compress
      - loopback.timeout
      - loopback.cookieParser
      - loopback.cookieSession
      - loopback.csrf
      - loopback.errorHandler
      - loopback.session
      - loopback.methodOverride
      - loopback.logger
      - loopback.responseTime
      - loopback.favicon
      - loopback.directory
      - loopback.vhost
    
    Also remove `loopback.mime`, which was set to `undefined` anyways.

--- server/middleware/favicon.js
+++ server/middleware/favicon.js
@@ -3,8 +3,14 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
+var favicon = require('serve-favicon');
+var path = require('path');
+
 /**
  * Serve the LoopBack favicon.
  * @header loopback.favicon()
  */
-module.exports = require('../../lib/express-middleware').favicon;
+module.exports = function(icon, options) {
+  icon = icon || path.join(__dirname, '../../favicon.ico');
+  return favicon(icon, options);
+};
