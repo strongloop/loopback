8235671b4c4676ab4bed7de8b06277f150233068
deepakrkris <deepak.r.kris@gmail.com>
2016-07-18 16:02:38 -0400

    updated for compile error
    allowExternalTTL -> allowEternalTTL

--- common/models/access-token.js
+++ common/models/access-token.js
@@ -150,7 +150,7 @@ module.exports = function(AccessToken) {
       var created = this.created.getTime();
       var elapsedSeconds = (now - created) / 1000;
       var secondsToLive = this.ttl;
-      var isNonExpiringToken = (secondsToLive === -1 && this.constructor.settings.allowExternalTTL);
+      var isNonExpiringToken = (secondsToLive === -1 && this.constructor.settings.allowEternalTTL);
       var isValid = (isNonExpiringToken) || (elapsedSeconds < secondsToLive);
 
       if (isValid) {
