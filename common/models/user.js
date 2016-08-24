5567917c12508c1ec7c4720d0689aede5698ac97
Loay <loay@users.noreply.github.com>
2016-08-24 16:30:58 -0400

    Allow resetPassword if  emailVerified

--- common/models/user.js
+++ common/models/user.js
@@ -576,6 +576,13 @@ module.exports = function(User) {
       }
       // create a short lived access token for temp login to change password
       // TODO(ritch) - eventually this should only allow password change
+      if (UserModel.settings.emailVerificationRequired && !user.emailVerified) {
+        err = new Error(g.f('Email has not been verified'));
+        err.statusCode = 401;
+        err.code = 'RESET_FAILED_EMAIL_NOT_VERIFIED';
+        return cb(err);
+      }
+
       user.accessTokens.create({ ttl: ttl }, function(err, accessToken) {
         if (err) {
           return cb(err);
