659e9ce09b4699ed700ff462a3baacc440bc3d05
박대선 <yume@yumes.net>
2016-12-23 14:04:44 +0900

    Fix false emailVerified on user model update
    We noticed that every time the user model updates, the emailVerified
    column would change to false, even though the email was not changed
    at all.
    
    I took a look and realized there might be an error in
    https://github.com/strongloop/loopback/commit/eb640d8
    
    The intent of the commit just mention is to make emailVerified false
    when the email gets changed, but notice that ctx.data.email is null
    on updates, so the condition is always met and emailVerified always
    becomes false.
    
    This commit fixes the issue just mentioned.

--- common/models/user.js
+++ common/models/user.js
@@ -845,7 +845,7 @@ module.exports = function(User) {
         if (emailChanged && ctx.Model.settings.emailVerificationRequired) {
           ctx.instance.emailVerified = false;
         }
-      } else {
+      } else if (ctx.data.email) {
         emailChanged = ctx.hookState.originalUserData.some(function(data) {
           return data.email != ctx.data.email;
         });
