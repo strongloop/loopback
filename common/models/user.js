dd7f18b7ac5be216eaab6b4dba960298701bb56c
Raeef Refai <raeef-refai@users.noreply.github.com>
2017-01-24 02:32:42 +0200

    Update user.js
    if no token provided for logout, then we set tokenID to empty string to throw a normal error "Error: access_token is a required argument" instead of "Error: Value is not a string"

--- common/models/user.js
+++ common/models/user.js
@@ -758,7 +758,7 @@ module.exports = function(User) {
             var accessToken = req && req.accessToken;
             var tokenID = accessToken && accessToken.id;
 
-            return tokenID;
+            return tokenID || '';
           }, description: 'Do not supply this argument, it is automatically extracted ' +
             'from request headers.',
           },
