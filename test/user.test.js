844147517e0496d5569652fea2d9113ca90e7dbe
ebarault <eric.barault@gmail.com>
2017-03-09 13:34:58 +0100

    first commit for general discussion

--- test/user.test.js
+++ test/user.test.js
@@ -2442,7 +2442,7 @@ describe('User', function() {
     it('handles subclassed user with no accessToken relation', () => {
       // setup a new LoopBack app, we don't want to use shared models
       app = loopback({localRegistry: true, loadBuiltinModels: true});
-      app.set('_verifyAuthModelRelations', false);
+      app.set('_disableAuthModelRelationsWarnings', true);
       app.set('remoting', {errorHandler: {debug: true, log: false}});
       app.dataSource('db', {connector: 'memory'});
       const User = app.registry.createModel({
