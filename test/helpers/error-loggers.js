4ebc517a7846172a7eea49da67407a3ddb3cd624
Miroslav Bajtoš <mbajtoss@gmail.com>
2017-10-19 13:08:54 +0200

    test: extract helpers for logging HTTP errors
    Extract two helpers into a shared file:
    
     - logAllServerErrors(app)
     - logServerErrorsOtherThan(statusCode, app)

---
+++ test/helpers/error-loggers.js
@@ -0,0 +1,21 @@
+// Copyright IBM Corp. 2015,2016. All Rights Reserved.
+// Node module: loopback
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+'use strict';
+
+exports.logAllServerErrors = function(app) {
+  exports.logServerErrorsOtherThan(-1, app);
+};
+
+exports.logServerErrorsOtherThan = function(statusCode, app) {
+  app.use((err, req, res, next) => {
+    if ((err.statusCode || 500) !== statusCode) {
+      console.log('Unhandled error for request %s %s: %s',
+        req.method, req.url, err.stack || err);
+    }
+    res.statusCode = err.statusCode || 500;
+    res.json(err);
+  });
+};
